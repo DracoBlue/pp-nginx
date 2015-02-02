206eb499d6aa635e7a41f56e9f0d4f0b0d31a5f6
DracoBlue <JanS@DracoBlue.de>
2015-02-02 13:13:01 +0100

    Fixed another typo in variables refs #17

--- tests/simple.example.org.pp
+++ tests/simple.example.org.pp
@@ -4,8 +4,8 @@ $server_config_file_name = "/tmp/pp-nginx-results/${domain_name}.conf"
 nginx::server { $domain_name:
   ensure => present,
   server_config_file_name => $server_config_file_name,
-  content => 'listen                *:80;
-server_name           simple.example.org;
+  content => "listen                *:80;
+server_name           ${domain_name};
 access_log            /var/log/nginx/simple.example.org.access.log;
-error_log             /var/log/nginx/simple.example.org.error.log;'
+error_log             /var/log/nginx/simple.example.org.error.log;"
 }
