f1ff301312dbea4abfed7d34fcd2853fc3d24981
DracoBlue <JanS@DracoBlue.de>
2015-02-02 12:30:29 +0100

    Fixed some warnings about " without variables for puppet lint. refs #17

--- tests/access-location-with-multiple-blocks.example.org.pp
+++ tests/access-location-with-multiple-blocks.example.org.pp
@@ -9,28 +9,28 @@
 #   deny    all;
 # }
 
-$domain_name = "access-location-with-multiple-blocks.example.org"
-$server_config_file_name = "/tmp/pp-nginx-results/$domain_name.conf"
+$domain_name = 'access-location-with-multiple-blocks.example.org'
+$server_config_file_name = '/tmp/pp-nginx-results/$domain_name.conf'
 
 nginx::server { $domain_name:
   ensure => present,
   server_config_file_name => $server_config_file_name,
-  content => "listen                *:80;
-server_name           $domain_name;"
+  content => 'listen                *:80;
+server_name           $domain_name;'
 }
 
-nginx::server::location { "with-multiple-blocks":
-  location => "~ /fives/",
+nginx::server::location { 'with-multiple-blocks':
+  location => '~ /fives/',
   server => Nginx::Server[$domain_name],
 }
 
-nginx::server::location::access { "block-one-with-multiple-blocks":
-  location => Nginx::Server::Location["with-multiple-blocks"],
+nginx::server::location::access { 'block-one-with-multiple-blocks':
+  location => Nginx::Server::Location['with-multiple-blocks'],
   deny => ['192.168.1.1']
 }
 
-nginx::server::location::access { "block-two-with-multiple-blocks":
-  location => Nginx::Server::Location["with-multiple-blocks"],
+nginx::server::location::access { 'block-two-with-multiple-blocks':
+  location => Nginx::Server::Location['with-multiple-blocks'],
   allow => ['192.168.1.0/24', '10.1.1.0/16', '2620:100:e000::8001'],
   deny => ['all']
 }
